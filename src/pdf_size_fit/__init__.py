cee28a1c3289e7eeae07a9262f11460a010a6992
Sumio Nishioka <oimus1976@gmail.com>
2026-08-27 06:01:59 +0900

    feat: export image-fit API

--- src/pdf_size_fit/__init__.py
+++ src/pdf_size_fit/__init__.py
@@ -1,5 +1,14 @@
-"""Experimental PDF size-fit diagnostics."""
+"""Experimental PDF size-fit diagnostics and route PoCs."""
 
 from .diagnose import Diagnosis, Route, diagnose_pdf
+from .image_fit import ImageFitAttempt, ImageFitResult, ImageFitStatus, fit_image_heavy_pdf
 
-__all__ = ["Diagnosis", "Route", "diagnose_pdf"]
+__all__ = [
+    "Diagnosis",
+    "Route",
+    "diagnose_pdf",
+    "ImageFitAttempt",
+    "ImageFitResult",
+    "ImageFitStatus",
+    "fit_image_heavy_pdf",
+]
