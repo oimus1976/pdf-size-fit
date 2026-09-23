9db0a31880ac162370c0dc66efd580184f6f8767
Sumio Nishioka <oimus1976@gmail.com>
2026-09-23 14:19:09 +0900

    feat: add split search progress phase (#35)

--- src/pdf_size_fit/progress.py
+++ src/pdf_size_fit/progress.py
@@ -9,6 +9,7 @@ class ProgressPhase(str, Enum):
     IMAGE_OPTIMIZATION = "image_optimization"
     PAGE_OPTIMIZATION = "page_optimization"
     HIGH_QUALITY_SEARCH = "high_quality_search"
+    SPLIT_SEARCH = "split_search"
 
 
 @dataclass(frozen=True)
